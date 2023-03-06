c765a7a1aa19d34d4a7baa124e53943b940ecf17
choisj <csj7480@gmail.com>
2023-03-06 10:03:56 +0900

    Use WandB

--- train_deform.py
+++ train_deform.py
@@ -5,32 +5,36 @@ import random
 import numpy as np
 import torch
 import torch.nn.functional as F
-import tensorflow as tf
 from lib.network import DeformNet
 from lib.loss import Loss
 from data.pose_dataset import PoseDataset
 from lib.utils import setup_logger, compute_sRT_errors
 from lib.align import estimateSimilarityTransform
 
+from tqdm import tqdm
+import wandb
+
+wandb.init(project='object-deform')
+wandb.run.name = 'origin'
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--dataset', type=str, default='CAMERA', help='CAMERA or CAMERA+Real')
+parser.add_argument('--dataset', type=str, default='CAMERA+Real', help='CAMERA or CAMERA+Real')
 parser.add_argument('--data_dir', type=str, default='data', help='data directory')
 parser.add_argument('--n_pts', type=int, default=1024, help='number of foreground points')
 parser.add_argument('--n_cat', type=int, default=6, help='number of object categories')
 parser.add_argument('--nv_prior', type=int, default=1024, help='number of vertices in shape priors')
 parser.add_argument('--img_size', type=int, default=192, help='cropped image size')
-parser.add_argument('--batch_size', type=int, default=32, help='batch size')
+parser.add_argument('--batch_size', type=int, default=64, help='batch size')
 parser.add_argument('--num_workers', type=int, default=10, help='number of data loading workers')
 parser.add_argument('--gpu', type=str, default='0', help='GPU to use')
 parser.add_argument('--lr', type=float, default=0.0001, help='initial learning rate')
 parser.add_argument('--start_epoch', type=int, default=1, help='which epoch to start')
-parser.add_argument('--max_epoch', type=int, default=50, help='max number of epochs to train')
+parser.add_argument('--max_epoch', type=int, default=25, help='max number of epochs to train')
 parser.add_argument('--resume_model', type=str, default='', help='resume from saved model')
-parser.add_argument('--result_dir', type=str, default='results/camera', help='directory to save train results')
+parser.add_argument('--result_dir', type=str, default='results/camera_real', help='directory to save train results')
 opt = parser.parse_args()
 
-opt.decay_epoch = [0, 10, 20, 30, 40]
+opt.decay_epoch = [0, 5, 10, 15, 20]
 opt.decay_rate = [1.0, 0.6, 0.3, 0.1, 0.01]
 opt.corr_wt = 1.0
 opt.cd_wt = 5.0
@@ -42,7 +46,6 @@ def train_net():
     # set result directory
     if not os.path.exists(opt.result_dir):
         os.makedirs(opt.result_dir)
-    tb_writer = tf.summary.FileWriter(opt.result_dir)
     logger = setup_logger('train_log', os.path.join(opt.result_dir, 'log.txt'))
     for key, value in vars(opt).items():
         logger.info(key + ': ' + str(value))
@@ -104,44 +107,48 @@ def train_net():
         train_dataloader = torch.utils.data.DataLoader(train_dataset, batch_size=opt.batch_size, sampler=train_sampler,
                                                        num_workers=opt.num_workers, pin_memory=True)
         estimator.train()
-        for i, data in enumerate(train_dataloader, 1):
-            points, rgb, choose, cat_id, model, prior, sRT, nocs = data
-            points = points.cuda()
-            rgb = rgb.cuda()
-            choose = choose.cuda()
-            cat_id = cat_id.cuda()
-            model = model.cuda()
-            prior = prior.cuda()
-            sRT = sRT.cuda()
-            nocs = nocs.cuda()
-            assign_mat, deltas = estimator(points, rgb, choose, cat_id, prior)
-            loss, corr_loss, cd_loss, entropy_loss, deform_loss = criterion(assign_mat, deltas, prior, nocs, model)
-            optimizer.zero_grad()
-            loss.backward()
-            optimizer.step()
-            global_step += 1
-            # write results to tensorboard
-            summary = tf.Summary(value=[tf.Summary.Value(tag='learning_rate', simple_value=current_lr),
-                                        tf.Summary.Value(tag='train_loss', simple_value=loss),
-                                        tf.Summary.Value(tag='corr_loss', simple_value=corr_loss),
-                                        tf.Summary.Value(tag='cd_loss', simple_value=cd_loss),
-                                        tf.Summary.Value(tag='entropy_loss', simple_value=entropy_loss),
-                                        tf.Summary.Value(tag='deform_loss', simple_value=deform_loss)])
-            tb_writer.add_summary(summary, global_step)
-            if i % 10 == 0:
-                logger.info('Batch {0} Loss:{1:f}, corr_loss:{2:f}, cd_loss:{3:f}, entropy_loss:{4:f}, deform_loss:{5:f}'.format(
-                    i, loss.item(), corr_loss.item(), cd_loss.item(), entropy_loss.item(), deform_loss.item()))
+        # for i, data in tqdm(enumerate(train_dataloader, 1)):
+        with tqdm(train_dataloader, unit='batch') as tepoch:
+            for i,data in enumerate(tepoch):
+                tepoch.set_description(f"Epoch {epoch}")
+                points, rgb, choose, cat_id, model, prior, sRT, nocs = data
+                points = points.cuda()
+                rgb = rgb.cuda()
+                choose = choose.cuda()
+                cat_id = cat_id.cuda()
+                model = model.cuda()
+                prior = prior.cuda()
+                sRT = sRT.cuda()
+                nocs = nocs.cuda()
+                assign_mat, deltas = estimator(points, rgb, choose, cat_id, prior)
+                loss, corr_loss, cd_loss, entropy_loss, deform_loss = criterion(assign_mat, deltas, prior, nocs, model)
+                optimizer.zero_grad()
+                wandb.log({
+                    'learning_rate' : current_lr,
+                    'train_loss' : loss,
+                    'corr_loss' : corr_loss,
+                    'cd_loss' : cd_loss,
+                    'entropy_loss' : entropy_loss,
+                    'deform_loss' : deform_loss
+                })
+                loss.backward()
+                optimizer.step()
+                global_step += 1
+                tepoch.set_postfix(loss=loss.item(),corr_loss=corr_loss.item(), cd_loss=cd_loss.item(), entropy_loss=entropy_loss.item(), deform_loss=deform_loss.item())
 
         logger.info('>>>>>>>>----------Epoch {:02d} train finish---------<<<<<<<<'.format(epoch))
-
+        # save model after each epoch
+        torch.save(estimator.state_dict(), '{0}/model_{1:02d}.pth'.format(opt.result_dir, epoch))
         # evaluate one epoch
         logger.info('Time {0}'.format(time.strftime("%Hh %Mm %Ss", time.gmtime(time.time() - st_time)) +
                     ', ' + 'Epoch %02d' % epoch + ', ' + 'Testing started'))
         val_loss = 0.0
         total_count = np.zeros((opt.n_cat,), dtype=int)
-        strict_success = np.zeros((opt.n_cat,), dtype=int)    # 5 degree and 5 cm
-        easy_success = np.zeros((opt.n_cat,), dtype=int)      # 10 degree and 5 cm
-        iou_success = np.zeros((opt.n_cat,), dtype=int)       # relative scale error < 0.1
+        strict_success = np.zeros((opt.n_cat,), dtype=int)           # 5 degree and 5 cm
+        strict_easy_success = np.zeros((opt.n_cat,), dtype=int)      # 5 degree and 10 cm
+        easy_success = np.zeros((opt.n_cat,), dtype=int)             # 10 degree and 5 cm
+        easy_easy_success = np.zeros((opt.n_cat,), dtype=int)        # 10 degree and 10 cm
+        iou_success = np.zeros((opt.n_cat,), dtype=int)              # relative scale error < 0.1
         # sample validation subset
         val_size = 1500
         val_idx = random.sample(list(range(val_dataset.length)), val_size)
@@ -149,70 +156,80 @@ def train_net():
         val_dataloader = torch.utils.data.DataLoader(val_dataset, batch_size=1, sampler=val_sampler,
                                                      num_workers=opt.num_workers, pin_memory=True)
         estimator.eval()
-        for i, data in enumerate(val_dataloader, 1):
-            points, rgb, choose, cat_id, model, prior, sRT, nocs = data
-            points = points.cuda()
-            rgb = rgb.cuda()
-            choose = choose.cuda()
-            cat_id = cat_id.cuda()
-            model = model.cuda()
-            prior = prior.cuda()
-            sRT = sRT.cuda()
-            nocs = nocs.cuda()
-            assign_mat, deltas = estimator(points, rgb, choose, cat_id, prior)
-            loss, _, _, _, _ = criterion(assign_mat, deltas, prior, nocs, model)
-            # estimate pose and scale
-            inst_shape = prior + deltas
-            assign_mat = F.softmax(assign_mat, dim=2)
-            nocs_coords = torch.bmm(assign_mat, inst_shape)
-            nocs_coords = nocs_coords.detach().cpu().numpy()[0]
-            points = points.cpu().numpy()[0]
-            # use choose to remove repeated points
-            choose = choose.cpu().numpy()[0]
-            _, choose = np.unique(choose, return_index=True)
-            nocs_coords = nocs_coords[choose, :]
-            points = points[choose, :]
-            _, _, _, pred_sRT = estimateSimilarityTransform(nocs_coords, points)
-            # evaluate pose
-            cat_id = cat_id.item()
-            if pred_sRT is not None:
-                sRT = sRT.detach().cpu().numpy()[0]
-                R_error, T_error, IoU = compute_sRT_errors(pred_sRT, sRT)
-                if R_error < 5 and T_error < 0.05:
-                    strict_success[cat_id] += 1
-                if R_error < 10 and T_error < 0.05:
-                    easy_success[cat_id] += 1
-                if IoU < 0.1:
-                    iou_success[cat_id] += 1
-            total_count[cat_id] += 1
-            val_loss += loss.item()
-            if i % 100 == 0:
-                logger.info('Batch {0} Loss:{1:f}'.format(i, loss.item()))
+        with tqdm(val_dataloader, unit='batch') as tepoch:
+            for i,data in enumerate(tepoch):
+                points, rgb, choose, cat_id, model, prior, sRT, nocs = data
+                points = points.cuda()
+                rgb = rgb.cuda()
+                choose = choose.cuda()
+                cat_id = cat_id.cuda()
+                model = model.cuda()
+                prior = prior.cuda()
+                sRT = sRT.cuda()
+                nocs = nocs.cuda()
+                assign_mat, deltas = estimator(points, rgb, choose, cat_id, prior)
+                loss, _, _, _, _ = criterion(assign_mat, deltas, prior, nocs, model)
+                # estimate pose and scale
+                inst_shape = prior + deltas
+                assign_mat = F.softmax(assign_mat, dim=2)
+                nocs_coords = torch.bmm(assign_mat, inst_shape)
+                nocs_coords = nocs_coords.detach().cpu().numpy()[0]
+                points = points.cpu().numpy()[0]
+                # use choose to remove repeated points
+                choose = choose.cpu().numpy()[0]
+                _, choose = np.unique(choose, return_index=True)
+                nocs_coords = nocs_coords[choose, :]
+                points = points[choose, :]
+                _, _, _, pred_sRT = estimateSimilarityTransform(nocs_coords, points)
+                # evaluate pose
+                cat_id = cat_id.item()
+                if pred_sRT is not None:
+                    sRT = sRT.detach().cpu().numpy()[0]
+                    R_error, T_error, IoU = compute_sRT_errors(pred_sRT, sRT)
+                    if R_error < 5 and T_error < 0.05:
+                        strict_success[cat_id] += 1
+                    if R_error < 5 and T_error < 0.1:
+                        strict_easy_success[cat_id] += 1
+                    if R_error < 10 and T_error < 0.1:
+                        easy_easy_success[cat_id] += 1
+                    if R_error < 10 and T_error < 0.05:
+                        easy_success[cat_id] += 1
+                    if IoU < 0.1:
+                        iou_success[cat_id] += 1
+                total_count[cat_id] += 1
+                val_loss += loss.item()
+                tepoch.set_postfix(loss=loss.item())
         # compute accuracy
         strict_acc = 100 * (strict_success / total_count)
+        strict_easy_acc = 100 * (strict_easy_success / total_count)
         easy_acc = 100 * (easy_success / total_count)
+        easy_easy_acc = 100 * (easy_easy_success / total_count)
         iou_acc = 100 * (iou_success / total_count)
         for i in range(opt.n_cat):
             logger.info('{} accuracies:'.format(val_dataset.cat_names[i]))
             logger.info('5^o 5cm: {:4f}'.format(strict_acc[i]))
+            logger.info('5^o 10cm: {:4f}'.format(strict_easy_acc[i]))
             logger.info('10^o 5cm: {:4f}'.format(easy_acc[i]))
+            logger.info('10^o 10cm: {:4f}'.format(easy_easy_acc[i]))
             logger.info('IoU < 0.1: {:4f}'.format(iou_acc[i]))
         strict_acc = np.mean(strict_acc)
+        strict_easy_acc = np.mean(strict_easy_acc)
         easy_acc = np.mean(easy_acc)
+        easy_easy_acc = np.mean(easy_easy_acc)
         iou_acc = np.mean(iou_acc)
         val_loss = val_loss / val_size
-        summary = tf.Summary(value=[tf.Summary.Value(tag='val_loss', simple_value=val_loss),
-                                    tf.Summary.Value(tag='5^o5cm_acc', simple_value=strict_acc),
-                                    tf.Summary.Value(tag='10^o5cm_acc', simple_value=easy_acc),
-                                    tf.Summary.Value(tag='iou_acc', simple_value=iou_acc)])
-        tb_writer.add_summary(summary, global_step)
+        wandb.log({
+                'val_loss' : current_lr,
+                '5^o5cm_acc' : strict_acc,
+                '5^o10cm_acc' : strict_easy_acc,
+                '10^o5cm_acc' : easy_acc,
+                '10^o10cm_acc' : easy_easy_acc,
+                'iou_acc' : iou_acc,
+            })
         logger.info('Epoch {0:02d} test average loss: {1:06f}'.format(epoch, val_loss))
         logger.info('Overall accuracies:')
-        logger.info('5^o 5cm: {:4f} 10^o 5cm: {:4f} IoU: {:4f}'.format(strict_acc, easy_acc, iou_acc))
+        logger.info('5^o 5cm: {:4f} 5^o 10cm: {:4f} 10^o 5cm: {:4f} 10^o 10cm: {:4f} IoU: {:4f}'.format(strict_acc, strict_easy_acc, easy_acc, easy_easy_acc, iou_acc))
         logger.info('>>>>>>>>----------Epoch {:02d} test finish---------<<<<<<<<'.format(epoch))
-        # save model after each epoch
-        torch.save(estimator.state_dict(), '{0}/model_{1:02d}.pth'.format(opt.result_dir, epoch))
-
 
 if __name__ == '__main__':
     train_net()
